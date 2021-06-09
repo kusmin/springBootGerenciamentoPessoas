7ceb05074f35643a978f8cbe669890ee3ecbe327
Renan Ribeiro <renan@itexto.com.br>
2021-06-09 11:08:27 -0300

    ajustes

---
+++ src/main/java/one/digitalinovation/personapi/entity/Person.java
@@ -0,0 +1,39 @@
+package one.digitalinovation.personapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    private LocalDate birthday;
+
+    @Column(nullable = false)
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    private List<Phone> phones;
+
+}
---
+++ src/main/java/one/digitalinovation/personapi/repository/PersonRepository.java
@@ -0,0 +1,9 @@
+package one.digitalinovation.personapi.repository;
+
+import one.digitalinovation.personapi.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+
+}
